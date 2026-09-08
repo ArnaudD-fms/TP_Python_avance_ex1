2da35b1b65265ae26101509429d1e2777d6f3d02
Arnaud Delperier <arnaud.delperier@fms-ea.com>
2026-09-08 14:39:49 +0200

    ajout des cas de test update et delete dans test_course_dao

--- tests/test_course_dao.py
+++ tests/test_course_dao.py
@@ -1,8 +1,6 @@
 import unittest
+
 from datetime import date
-
-import pymysql
-
 from daos.course_dao import CourseDao
 from models.course import Course
 from models.teacher import Teacher
@@ -29,7 +27,10 @@ class TestCourseDao(unittest.TestCase):
         self.assertIsNone(course)
 
     def test_create_course(self):
-        course = Course("Cuisine", date(2026, 9, 8), date(2026, 9, 9))
+        course = Course("Creation_test", date(2026, 9, 8), date(2026, 9, 9))
+
+        # On créer un teacher car on a besoin que le cours possède un teacher et un teacher.id
+        # Les attributs du teacher en lui-même n'ont pas d'importance seul l'id compte
         teacher = Teacher("Test", "Test", 99, date(2026, 1, 1))
         teacher.id = 1
         course.teacher = teacher
@@ -41,13 +42,13 @@ class TestCourseDao(unittest.TestCase):
         self.assertEqual(result, course.id)
 
     def test_create_course_no_associaite_teacher(self):
-        course = Course("Cuisine", date(2026, 9, 8), date(2026, 9, 9))
+        course = Course("Creation_test", date(2026, 9, 8), date(2026, 9, 9))
 
         result = self.course_dao.create(course)
         self.assertEqual(result, 0)
 
     def test_create_course_with_unknown_teacher(self):
-        course = Course("Cuisine", date(2026, 9, 8), date(2026, 9, 9))
+        course = Course("Creation_test", date(2026, 9, 8), date(2026, 9, 9))
         teacher = Teacher("Test", "Test", 99, date(2026, 1, 1))
         teacher.id = 99
         course.teacher = teacher
@@ -55,6 +56,55 @@ class TestCourseDao(unittest.TestCase):
         result = self.course_dao.create(course)
         self.assertEqual(result, 0)
 
+    def test_update_course(self):
+        # Création d'un nouveau cours
+        teacher = Teacher("Test", "Test", 99, date(2026, 1, 1))
+        teacher.id = 1
+        course = Course(
+            "Test",
+            date(2026, 9, 8),
+            date(2026, 9, 9)
+        )
+        course.teacher = teacher
+        course_id = self.course_dao.create(course)
+
+        # On vérifie que la création a fonctionné
+        self.assertNotEqual(course_id, 0)
+
+        # On modifie l'objet cours
+        course.name = "Update_test"
+        course.start_date = date(2026, 10, 3)
+        course.end_date = date(2026, 10, 5)
+
+        # On vérifie que l'update a fonctionné
+        result = self.course_dao.update(course)
+        self.assertTrue(result)
+
+        # On vérifie que la ligne en base a bien été modifié
+        updated_course = self.course_dao.read(course_id)
+        self.assertEqual(updated_course.name, "Update_test")
+        self.assertEqual(updated_course.start_date, date(2026, 10, 3))
+        self.assertEqual(updated_course.end_date, date(2026, 10, 5))
+
+    def test_delete_course(self):
+        teacher = Teacher("Test", "Test", 99, date(2026, 1, 1))
+        teacher.id = 1
+
+        # Création d'un nouveau cours
+        course = Course(
+            "Delete_test",
+            date(2026, 9, 8),
+            date(2026, 9, 9)
+        )
+        course.teacher = teacher
+        course_id = self.course_dao.create(course)
+        self.assertNotEqual(course_id, 0)
+
+        # Suppression du cours
+        result = self.course_dao.delete(course)
+        self.assertTrue(result)
+        deleted_course = self.course_dao.read(course_id)
+        self.assertIsNone(deleted_course)
 
     if __name__ == '__main__':
         unittest.main()
